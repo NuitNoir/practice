371c1e20311ff3fc6ef6543439420e778dfd7414
kyota_practice_pc <nuit.noir.8@gmail.com>
2015-04-27 17:32:15 +0900

    hough transform modify

--- asign0423/hough.cpp
+++ asign0423/hough.cpp
@@ -8,15 +8,15 @@ void Hough(cv::Mat&);
 
 int main() {
 	cv::Mat src = cv::imread("building.jpg", 0);
-
+	//cv::Mat src = cv::imread("line.png", 0);
 	//src.channels() = 1;
 	Hough(src);
 }
 
 void Hough(cv::Mat &src) {
 	//cv::vector lines;
-	int height = 320, width = 480;
-	int x0, y0, x1, y1, x2, y2;
+	int height = src.rows, width = src.cols;
+	//int x0, y0, x1, y1, x2, y2;
 	cv::vector<cv::Vec2f> lines;
 	cv::Mat dst, cdst;
 	cv::Canny(src, dst, 50, 200, 3);
@@ -26,21 +26,24 @@ void Hough(cv::Mat &src) {
 
   //imshow("source", src);
 
-	cv::HoughLines(src, lines, 1.0, CV_PI/180, 150, 0, 0 );
+	cv::HoughLines(src, lines, 15.0, CV_PI/180, 150);
 
 	std::cout << lines.size() << std::endl;
 	for (int i=0; i<(int)lines.size(); i++) {
-		double rho = lines[i][0];
+		double r = lines[i][0];
 		double theta = lines[i][1];
 		double tan = sin(theta)/cos(theta);
-		x0 = rho*cos(theta);
-		y0 = rho*sin(theta);
-		x1 = x0 + y0*(1/tan);
-		y1 = y0 + x0*(tan);
-		x2 = x0 + (height - y1)*(1/tan);
-		y2 = y0 + (width - x1)*(tan);
+		cv::Point p1;
+		cv::Point p2;
+		p1.x = r/cos(theta);
+		p1.y = 0;
+		p2.x = 0;
+		p2.y = r/sin(theta);
 
-		draw_line(src, x1, y1, x2, y2, 128);
+		//std::cout << lines[i] << std::endl;
+		//std::cout << x1 << ' ' << y1 << ' '<< x2<<' ' << y2<<' ' << std::endl;
+		cv::line(src, p1, p2, 10);
+		//draw_line(src, x1, y1, x2, y2, 1);
 		//draw_line(src, lines[i][0], lines[i][1], lines[i][2], lines[i][3], 1);
 	}
 
