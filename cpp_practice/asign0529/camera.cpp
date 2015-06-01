0252c0e6911aac4901117002b0dbad2a47e03823
kyota_practice_pc <nuit.noir.8@gmail.com>
2015-06-01 19:23:53 +0900

    add skel files

--- cpp_practice/asign0529/camera.cpp
+++ cpp_practice/asign0529/camera.cpp
@@ -11,18 +11,22 @@
 #include "camera.hpp"
 
 int main() {
-	////////// make skel
+	//////////    make skel
 	SKEL skel = SKEL("../asign0522/cg.skel");
 	SKEL skel2 = SKEL("world.skel");
 	SKEL skel3 = SKEL("T.skel");
-	SKEL skel4 = SKEL("T_.skel");
-	SKEL skel5 = SKEL("square.skel");
+	SKEL skel5 = SKEL("L1.skel");
+	SKEL skel6 = SKEL("L2.skel");
+	SKEL skel7= SKEL("L3.skel");
+	SKEL skel8 = SKEL("T2.skel");
 	vector<SKEL> skels;
-//	skels.push_back(skel);
+     skels.push_back(skel);
 	skels.push_back(skel2);
-	// skels.push_back(skel3);
-	// skels.push_back(skel4);
+	skels.push_back(skel3);
 	skels.push_back(skel5);
+	skels.push_back(skel6);
+	skels.push_back(skel7);
+	skels.push_back(skel8);
 	/////////// make camera
 	Camera c1;
 	Camera c2;
@@ -36,18 +40,18 @@ int main() {
 	Matx44d R2 = Translation(1, -10, -1);
 	R2 = R2*RotationY(3.14/2);
 	c1.set_location(R);
-	c2.set_location(R2);
+	// c2.set_location(R2);
 
 	Matx44d T  = Translation(0, 0, 0.1); 
-	Matx44d T2  = RotationX(0.01);
+	// Matx44d T2  = RotationX(0.01);
 	//R = R*T ;
 
 	for (unsigned i=0; i<100; i++) {
 		cv::Mat img(500, 500, CV_8UC1, 1);
 		c1.transform(T);
-		c2.transform(T2);
+		// c2.transform(T2);
 		//cout << i << endl;
-		draw_skels_perspective(img, skels, c1, 255);
+		c1.draw_skels_perspective(img, skels, (unsigned char)255);
 		Point2f P(1, 0);
 		Scalar S(255);
 		putText(img, "X", P, FONT_HERSHEY_SIMPLEX, 5, S);
--- cpp_practice/asign0529/camera.hpp
+++ cpp_practice/asign0529/camera.hpp
@@ -4,17 +4,16 @@
 #include <opencv2/opencv.hpp>
 #include "../asign0522/8_geometry4/skip_comments.hpp"
 #include "../asign0522/skel.hpp"
-#include "../asign0522/cg.hpp"
+// #include "../asign0522/cg.hpp"
 #include "../asign0522/transform_3d.hpp"
 
 using namespace tutor;
 using namespace std;
 using namespace cv;
 
-void draw_skel_perspective(Mat &img, const SKEL &skel, unsigned char gray);
 class Camera {
-	private:
-		unsigned _width, _height;
+private:
+	unsigned _width, _height;
 		Matx33d _intrinsic;  // inner parameter
 		Matx44d _location;		//	location and derection
 
@@ -31,8 +30,8 @@ class Camera {
 		}
 		Matx33d set_intrinsic(double f, double ku, double kv, double s, double u0, double v0) {
 			return _intrinsic = Matx33d(f*ku, f*s, u0,
-																	0, f*kv, v0,
-																	0, 0, 1);
+				0, f*kv, v0,
+				0, 0, 1);
 		}
 		void set_location(Matx44d M) {
 			_location = M;
@@ -40,111 +39,82 @@ class Camera {
 		void transform(Matx44d &T) {
 			_location = T*_location;
 		}
-		void draw_skels_perspective(Mat& img, const vector<SKEL>& skels, const Camera& C, unsigned char gray) {
+		void draw_skels_perspective(Mat& img, const vector<SKEL>& skels, unsigned char gray) {
 			Matx34d I0(1, 0, 0, 0,
-										0, 1, 0, 0,
-										0, 0, 1, 0);
+				0, 1, 0, 0,
+				0, 0, 1, 0);
 			Matx43d I1(1, 0, 0,
-										0, 1, 0,
-										0, 0, 1,
-										0, 0, 0);
-			Camera _c = C;
+				0, 1, 0,
+				0, 0, 1,
+				0, 0, 0);
+			// Camera _c = this;
 			for (unsigned i=0; i<skels.size(); i++) {
-				Matx44d R = I1*_c.get_intrinsic()*I0*_c.get_location();
+				Matx44d R = I1*this->get_intrinsic()*I0*this->get_location();
 				SKEL _skel = skels[i];
 				_skel.transform(R);
 				draw_skel_perspective(img, _skel, gray);
 			}
 		}
-		
-/*
-		void Rotate(Matx44d &R0) {
-			Matx44d T = _location;
-			Matx33d R(T(0,0), T(0,1), T(0,2),
-										T(1,0), T(1,1), T(1,2),
-										T(2,0), T(2,1), T(2,2));
-			Matx31d t(T(1,0), T(1,0), T(2,0));
-			R = R0*R;
-			t = R*t;
-			_location = Matx44d(R(0,0), R(0,1), R(0,2), t(0,0),
-																R(1,0), R(1,1), R(1,2), t(1,0),
-																R(2,0), R(2,1), R(2,2), t(2,0),
-																R(3,0), R(3,1), R(3,2), t(3,0));
-		}*/
-		/*
-		void Translate(Matx44d &T0) {
-			Matx44d T = _location;
-			Matx33d R(T(0,0), T(0,1), T(0,2),
-					T(1,0), T(1,1), T(1,2),
-					T(2,0), T(2,1), T(2,2));
-			Matx13d t(T(0,0), T(0,1), T(0,2));
-			R = T0*R;
-			t = R*t;
-			_location = Matx44d(R(0,0), R(0,1), R(0,2), t(0,0),
-					R(1,0), R(1,1), R(1,2), t(1,0),
-					R(2,0), R(2,1), R(2,2), t(2,0),
-					R(3,0), R(3,1), R(3,2), t(3,0));
-		}
-		*/
-};
 
+		void draw_skel_perspective(Mat &img, const SKEL &skel, unsigned char gray) {
+		  //skel.print_vertices();
+			vector<PPoint3d> vertices = skel.vertices();
+			vector<PPoint2d> projected_v;
 
-void draw_skels_perspective(Mat& img, const vector<SKEL>& skels, const Camera& C, unsigned char gray);
+			for (unsigned i=0; i < vertices.size(); i++) {
+		    //cout << vertices[i][0] << endl;
+				projected_v.push_back( perspective_projection(vertices[i]) );
+			}
 
-void draw_skels_perspective(Mat& img, const vector<SKEL>& skels, const Camera& C, unsigned char gray) {
-	Matx34d I0(1, 0, 0, 0,
-								0, 1, 0, 0,
-								0, 0, 1, 0);
-	Matx43d I1(1, 0, 0,
-								0, 1, 0,
-								0, 0, 1,
-								0, 0, 0);
-	Camera _c = C;
-	for (unsigned i=0; i<skels.size(); i++) {
-		Matx44d R = I1*_c.get_intrinsic()*I0*_c.get_location();
-		SKEL _skel = skels[i];
-		_skel.transform(R);
-		draw_skel_perspective(img, _skel, gray);
+			int hide_flag = 0;
+		  //skel.print_polylines();
+			vector<Polyline> lines = skel.polylines();
+			for (unsigned i=0; i<lines.size(); i++) {
+		    for (unsigned j=1; j<lines[i][0]; j++) { //lines[i][0] has passing point num.
+		      //draw_line(img, projected_v[lines[i][j]][0], projected_v[lines[i][j]][1],
+		      //		projected_v[lines[i][j+1]][0], projected_v[lines[i][j+1]][1], 255);
+		    	double f = 4, bias = 250;
+
+		      //cout << lines[i][1] << lines[i][2] << lines[i][3] << endl;
+		    	if (lines[i][0] >= 3) {
+		        //hide_flag = hide_line(projected_v[lines[i][1]], projected_v[lines[i][2]], projected_v[lines[i][3]] );
+		        //cout << hide_flag << endl;
+		    	}
+		      //if ( (projected_v[lines[i][j]][2] > EPS || projected_v[lines[i][j+1]][2] > EPS) && hide_flag != 1) {
+		    	if (hide_flag != 1) {
+		    		double p1x, p1y, p1z;
+		    		p1x = projected_v[lines[i][j]].x()*f + bias;
+		    		p1y = projected_v[lines[i][j]].y()*f + bias;
+		    		p1z = projected_v[lines[i][j]][2];
+		    		double p2x, p2y, p2z;
+		    		p2x = projected_v[lines[i][j+1]].x()*f + bias;
+		    		p2y = projected_v[lines[i][j+1]].y()*f + bias;
+		    		p2z = projected_v[lines[i][j]][2];
+		        //cout << p1x << ' ' << p1y << endl;
+		    		Point2f p1(p1x, p1y);
+		    		Point2f p2(p2x, p2y);
+		        // line(img, projected_v[lines[i][j]], projected_v[lines[i][j+1]], 255);
+		        // cout << p1 << ' ' << p2 << endl;
+		        // cout << p1z << ' ' << p2z << endl;
+		        cout << _location(2,3) << endl; 
+		        double z = _location(2,3);
+		        if (p1z < z && p2z < z) {
+		    		line(img, p1, p2, gray);
+		        }
+		    	}
+		    	hide_flag = 0;
+		    }
+		  }
 	}
-}
 
-void draw_skel_perspective(Mat &img, const SKEL &skel, unsigned char gray) {
-  //skel.print_vertices();
-  vector<PPoint3d> vertices = skel.vertices();
-  vector<PPoint2d> projected_v;
 
-  for (unsigned i=0; i < vertices.size(); i++) {
-    //cout << vertices[i][0] << endl;
-    projected_v.push_back( perspective_projection(vertices[i]) );
-  }
-
-  int hide_flag = 0;
-  //skel.print_polylines();
-  vector<Polyline> lines = skel.polylines();
-  for (unsigned i=0; i<lines.size(); i++) {
-    for (unsigned j=1; j<lines[i][0]; j++) { //lines[i][0] has passing point num.
-      //draw_line(img, projected_v[lines[i][j]][0], projected_v[lines[i][j]][1],
-      //		projected_v[lines[i][j+1]][0], projected_v[lines[i][j+1]][1], 255);
-      double f = 4, bias = 250;
-
-      //cout << lines[i][1] << lines[i][2] << lines[i][3] << endl;
-      if (lines[i][0] >= 3) {
-        //hide_flag = hide_line(projected_v[lines[i][1]], projected_v[lines[i][2]], projected_v[lines[i][3]] );
-        //cout << hide_flag << endl;
-      }
-      //if ( (projected_v[lines[i][j]][2] > EPS || projected_v[lines[i][j+1]][2] > EPS) && hide_flag != 1) {
-      if (hide_flag != 1) {
-        double p1x, p1y;
-        p1x = projected_v[lines[i][j]].x()*f + bias;
-        p1y = projected_v[lines[i][j]].y()*f + bias;
-        //cout << p1x << ' ' << p1y << endl;
-        Point2f p1(p1x, p1y);
-        Point2f p2(projected_v[lines[i][j+1]].x()*f + bias, projected_v[lines[i][j+1]].y()*f + bias);
-        //line(img, projected_v[lines[i][j]], projected_v[lines[i][j+1]], 255);
-        //cout << p1 << ' ' << p2 << endl;
-        line(img, p1, p2, gray);
-      }
-      hide_flag = 0;
-    }
-  }
-}
+	PPoint2d perspective_projection(const PPoint3d &p3d) {
+	  double f = 30;
+	  //cout << p3d[0] << endl;
+	  Matx34d M(f, 0, 0, 0,
+	      0,f, 0, 0,
+	      0, 0, 1, 0);
+	  PPoint2d p2d = M*p3d;
+	  return p2d;
+	}
+};
