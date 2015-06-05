6b5299c3d768010cb999d5587b4784d2ff1a3f9b
kyota_Mac <nuit.noir.8@gmail.com>
2015-06-05 10:31:36 +0900

    view volume の設定

--- cpp_practice/asign0529/camera.cpp
+++ cpp_practice/asign0529/camera.cpp
@@ -22,29 +22,32 @@ int main() {
 	vector<SKEL> skels;
   // skels.push_back(skel);
 	skels.push_back(skel2);
-	skels.push_back(skel3);
-	skels.push_back(skel5);
-	skels.push_back(skel6);
-	skels.push_back(skel7);
-	skels.push_back(skel8);
+	// skels.push_back(skel3);
+	// skels.push_back(skel5);
+	// skels.push_back(skel6);
+	// skels.push_back(skel7);
+	// skels.push_back(skel8);
+
 	/////////// make camera
 	Camera c1;
 	Camera c2;
 	c1.set_size(500, 500);
 	c2.set_size(500, 500);
-	c1.set_intrinsic(0.5, 3., 2., 0., 1., 1.); // f, ku, kv, s, u0, v0
+	c1.set_intrinsic(1, 1., 1., 0., 1., 1.); // f, ku, kv, s, u0, v0
 	// c2.set_intrinsic(2. , 1., 1., 0., 1., 1.);
+	double z0 = -10;
+	double dz = 0.1;
 
 	// make location
-	Matx44d R = Translation(-3, -3, 10);
+	Matx44d R = Translation(-1, -1, z0);
 	// Matx44d R2 = Translation(1, -10, -1);
-	Matx44d R3 = RotationY(-0.1);
+	Matx44d R3 = RotationY(0.1);
 	// R = R3*RotationY(3.14/2);
 	R = R3*R;
 	c1.set_location(R);
 	// c2.set_location(R2);
 
-	Matx44d T  = Translation(0, 0, -0.2);
+	Matx44d T  = Translation(0, 0, dz);
 	// Matx44d T2  = RotationX(0.01);
 	//R = R*T ;
 	for (unsigned i=0; i<100; i++) {
@@ -53,9 +56,9 @@ int main() {
 		// c2.transform(T2);
 		//cout << i << endl;
 		c1.draw_skels_perspective(img, skels, (unsigned char)255);
-		Point2f P(1, 0);
-		Scalar S(255);
-		putText(img, "X", P, FONT_HERSHEY_SIMPLEX, 5, S);
+		// Point2f P(1, 0);
+		// Scalar S(255);
+		// putText(img, "X", P, FONT_HERSHEY_SIMPLEX, 5, S);
 		stringstream file_name;
 		file_name <<"img/camera" << std::setw( 3 ) << std::setfill( '0' ) << i   << ".png";
 	  cv::imwrite(file_name.str(), img);
--- cpp_practice/asign0529/camera.hpp
+++ cpp_practice/asign0529/camera.hpp
@@ -56,7 +56,15 @@ private:
 				draw_skel_perspective(img, _skel, gray);
 			}
 		}
-
+		PPoint2d perspective_projection(const PPoint3d &p3d) {
+		  double f = 30;
+		  //cout << p3d[0] << endl;
+		  Matx34d M(f, 0, 0, 0,
+		      0,f, 0, 0,
+		      0, 0, 1, 0);
+		  PPoint2d p2d = M*p3d;
+		  return p2d;
+		}
 		void draw_skel_perspective(Mat &img, const SKEL &skel, unsigned char gray) {
 		  //skel.print_vertices();
 			vector<PPoint3d> vertices = skel.vertices();
@@ -71,7 +79,8 @@ private:
 		  //skel.print_polylines();
 			vector<Polyline> lines = skel.polylines();
 			for (unsigned i=0; i<lines.size(); i++) {
-		    for (unsigned j=1; j<lines[i][0]; j++) { //lines[i][0] has passing point num.
+		    for (unsigned j=1; j<lines[i][0]; j++) { 
+		    	//lines[i][0] has passing point num.
 		      //draw_line(img, projected_v[lines[i][j]][0], projected_v[lines[i][j]][1],
 		      //		projected_v[lines[i][j+1]][0], projected_v[lines[i][j+1]][1], 255);
 		    	double f = 4, bias = 250;
@@ -86,38 +95,72 @@ private:
  	    		double p1x, p1y, p1z;
 	    		p1x = projected_v[lines[i][j]].x()*f + bias;
 	    		p1y = projected_v[lines[i][j]].y()*f + bias;
-	    		p1z = projected_v[lines[i][j]][2];
+	    		// p1z = projected_v[lines[i][j]][2];
+	    		p1z = vertices[lines[i][j]][2];
+
 	    		double p2x, p2y, p2z;
 	    		p2x = projected_v[lines[i][j+1]].x()*f + bias;
 	    		p2y = projected_v[lines[i][j+1]].y()*f + bias;
-	    		p2z = projected_v[lines[i][j]][2];
-	        //cout << p1x << ' ' << p1y << endl;
+	    		// p2z = projected_v[lines[i][j+1]][2];
+	    		p2z = vertices[lines[i][j+1]][2];
+
 	    		Point2f p1(p1x, p1y);
 	    		Point2f p2(p2x, p2y);
-	        // line(img, projected_v[lines[i][j]], projected_v[lines[i][j+1]], 255);
-	        // cout << p1 << ' ' << p2 << endl;
-	        // cout << p1z << ' ' << p2z << endl;
-	        cout << img.cols << _location(2,3)<<' '<<p1z<<' '<<p2z << endl; 
+	        // cout << "p1="<<p1 << " p2="<< p2 << endl;
 	        double z = _location(2,3);
-	        if (p1z < z && p2z < z) continue;
+	        // cout << z << ' '<< p1z << ' ' << p2z << endl;
+	        // if (p1z > z || p2z < z) {
+	        // 	cout << _location(2,3)<<' '<<p1z<<' '<<p2z << endl; 
+	        // }
+	        // if (p1z < z && p2z < z) {
+		       //  cout << img.cols << _location(2,3)<<' '<<p1z<<' '<<p2z << endl;
+	        // 	continue;
+	        // }
+	        if (lines[i][0] >= 3) {
+        		// hide_flag = hide_line(projected_v[lines[i][1]], projected_v[lines[i][2]], projected_v[lines[i][3]] );
+       		 	//cout << hide_flag << endl;
+     		 	}
+  		    //if ( (projected_v[lines[i][j]][2] > EPS || projected_v[lines[i][j+1]][2] > EPS) && hide_flag != 1) {
+		      ///////// hide line
+		      // if (hide_flag == 1) {
+		      // 	hide_flag = 0;
+		      // 	continue; 
+		      // }
+		      cout << p1 << p2 << endl;
+	        if (p1z > 0 && p2z > 0) continue;
 	        if ((p1x > img.cols || p1x < 0 || p1y > img.rows || p1y < 0) && 
 	        	(p2x > img.cols || p2x < 0 || p2y > img.rows || p2y < 0)) {
 	        	continue;
-	        }	        	
+	        }
 	        // if (p2x > img.cols || p2x < 0 || p2y > img.rows || p2y < 0) continue;
-    			// line(img, p1, p2, gray);	
-    			draw_line(img, p1x, p1y, p2x, p2y, gray);
+    			line(img, p1, p2, gray);	
+    			// draw_line(img, p1x, p1y, p2x, p2y, gray);
 		    	hide_flag = 0;
 		    }
 		  }
 	}
-	PPoint2d perspective_projection(const PPoint3d &p3d) {
-	  double f = 30;
-	  //cout << p3d[0] << endl;
-	  Matx34d M(f, 0, 0, 0,
-	      0,f, 0, 0,
-	      0, 0, 1, 0);
-	  PPoint2d p2d = M*p3d;
-	  return p2d;
-	}
+/*
+ * ***  return value *************
+ * when hide line return 1 (true)
+ * when doesn't hide line return 0 (false)
+ * ****************************************
+ */
+	int hide_line(PPoint2d p0, PPoint2d p1, PPoint2d p2) {
+  // Point2f vec1(p1[0]-p0[0], p1[1]-p0[1]);
+  // Point2f vec2(p2[0]-p0[0], p2[1]-p0[1]);
+  Point2f vec1(p0[0]-p1[0], p0[1]-p1[1]);
+  Point2f vec2(p2[0]-p1[0], p2[1]-p1[1]);
+
+  // double inner_product = vec1.ddot(vec2);
+  double inner_product = vec1.x*vec2.x + vec1.y*vec2.y;
+
+  // cout << inner_product << vec1 << vec2 <<  endl;
+
+  if (inner_product < 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 };
+
